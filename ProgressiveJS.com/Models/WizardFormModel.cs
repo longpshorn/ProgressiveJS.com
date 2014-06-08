c8915256b0ecc2bbb6789d4d8d4d7d86bc5be939
longpshorn <patrick.j.smith1@gmail.com>
2014-06-08 07:09:48 -0500

    Reorganized demo page to make more modular.  Started work on adding wizard form example.

---
+++ ProgressiveJS.com/Models/WizardFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgressiveJS.com.Models
+{
+    public class WizardFormModel : DataFormModel
+    {
+        public WizardFormModel(int step = 1)
+        {
+            Step = step;
+        }
+
+        public int Step { get; set; }
+    }
+}
